18dccde3b9139620f199e381dd367e609bce08a9
shahing <arrestshaheen@gmail.com>
2024-06-21 19:14:11 +0100

    fifth code

---
+++ Practice/ShapeArea.js
@@ -0,0 +1,7 @@
+function shapeArea(n){
+    return n*n + (n-1)*(n-1) 
+}
+console.log(shapeArea(2)); // Output: 5
+console.log(shapeArea(3)); // Output: 13
+console.log(shapeArea(4)); // Output: 25
+console.log(shapeArea(1)); // Output: 1
